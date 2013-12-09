9c0d708a5588c8a2245cfedaab78ea1851f0f604
Joey Wendt <joey@joeywendt.com>
2013-12-09 14:15:22 -0600

    Add OctoExtractor#octokit_ruby_source_filepaths.

--- lib/octo_extractor.rb
+++ lib/octo_extractor.rb
@@ -1,2 +1,14 @@
-class OctoExtractor
+module OctoExtractor
+  class << self
+
+    # List all the Octokit ruby source filepaths for the latest installed
+    #   version.
+    #
+    # @return [Array<String>] List of Octokit source ruby filepaths
+    def octokit_ruby_source_filepaths
+      `gem content octokit`.split("\n").
+        select { |filepath| filepath[-3..-1] == ".rb" }
+    end
+
+  end
 end
--- test/test_octo_extractor.rb
+++ test/test_octo_extractor.rb
@@ -4,8 +4,10 @@ require_relative "../lib/octo_extractor"
 
 class TestOctoExtractor < Test::Unit::TestCase
 
-  def test_sanity
-    assert 1
+  def test_octokit_ruby_source_filepaths
+  	filepath = OctoExtractor.octokit_ruby_source_filepaths.first
+    assert filepath.include? "octokit"
+    assert_equal ".rb", filepath[-3..-1]
   end
 
 end
