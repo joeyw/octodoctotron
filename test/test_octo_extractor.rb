319ccf9f75cb17be4c3f97554b0e6786d56bc7f5
Joey Wendt <joey@joeywendt.com>
2013-12-09 16:28:08 -0600

    add OctoExtractor.process method.
    This is the initial file parser to extract the data we want from the Octokit
    source files. It is super janky right now and only works for a very simple
    test case. As more tests are added it will become more refined and refactored.

--- test/test_octo_extractor.rb
+++ test/test_octo_extractor.rb
@@ -10,4 +10,14 @@ class TestOctoExtractor < Test::Unit::TestCase
     assert_equal ".rb", filepath[-3..-1]
   end
 
+  def test_extact_api_url_and_method_name_from_simple_source
+  	expected_data = [{
+  		api_url: 'http://developer.github.com/v3/meta/#meta',
+  		method_name: 'meta'
+  	}]
+  	data = OctoExtractor.process('test/fixtures/octokit_meta.rb')
+
+  	assert_equal expected_data, data
+  end
+
 end
