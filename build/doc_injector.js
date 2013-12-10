d09636ff6f1e3366cab3f7ed19e732280f52022a
Joey Wendt <joey@joeywendt.com>
2013-12-09 22:09:56 -0600

    Imrove the build script
    Added extract data from octokit to the build
    The doc injector now builds with the data extracted from the above
    The bookmarklet is compiled from the doc injector generated above

---
+++ build/doc_injector.js
@@ -0,0 +1,17 @@
+var DocInjector = {
+	init: function() {
+		var that = this;
+		$.each(this.data, function(i, docReference) {
+			that.inject(docReference)
+		});
+	},
+	data: [{"api_url":"http://developer.github.com/v3/meta/","method_name":"meta"}],
+	inject: function(docReference) {
+		var anchor = docReference.api_url.split("#");
+		if(window.location.href !== anchor[0]) return;
+		var html = '<div><code>Octokit.rb: <a href="#" target="_blank">Octokit#' + docReference.method_name + '</a></code></div>';
+		$('#' + anchor[1]).after(html);
+	}
+};
+
+$().ready(function() { DocInjector.init(); });
