b4bf11ec7c16b055e3f95574ee2bd1336ce3438c
Joey Wendt <joey@joeywendt.com>
2013-12-11 16:39:15 -0600

    remove fixtures. test directly against octokit

--- test/test_octo_extractor.rb
+++ test/test_octo_extractor.rb
@@ -5,19 +5,25 @@ require_relative "../lib/octo_extractor"
 class TestOctoExtractor < Test::Unit::TestCase
 
   def test_octokit_ruby_source_filepaths
-  	filepath = OctoExtractor.filepaths.first
+    filepath = OctoExtractor.filepaths.first
     assert filepath.include? "octokit"
     assert_equal ".rb", filepath[-3..-1]
   end
 
   def test_extact_api_url_and_method_name_from_simple_source
-  	expected_data = [{
-  		api_url: 'http://developer.github.com/v3/meta/#meta',
-  		method_name: 'meta'
-  	}]
-  	data = OctoExtractor.process('test/fixtures/octokit_meta.rb')
-
-  	assert_equal expected_data, data
+    expected_data = [{
+      api_url: 'http://developer.github.com/v3/meta/#meta',
+      method_name: 'meta'
+    }]
+    data = OctoExtractor.process get_source_file('client/meta.rb')
+    assert_equal expected_data, data
   end
 
+  private
+
+  def get_source_file(file_name)
+    OctoExtractor.filepaths.select { |fp|
+      fp.match Regexp.quote file_name
+    }.first
+  end
 end
